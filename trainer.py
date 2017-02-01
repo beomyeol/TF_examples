3fc25b490b9c088f69932341b1e2c703da697378
Beomyeol Jeon <beomyeolj@gmail.com>
2017-02-01 15:06:43 -0600

    Update data parallel script

--- trainer.py
+++ trainer.py
@@ -12,9 +12,18 @@ tf.app.flags.DEFINE_string("worker_hosts", "",
 # Flags for defining the tf.train.Server
 tf.app.flags.DEFINE_string("job_name", "", "One of 'ps', 'worker'")
 tf.app.flags.DEFINE_integer("task_index", 0, "Index of task within the job")
+tf.app.flags.DEFINE_integer("steps", 100, "Number of training steps")
 
 FLAGS = tf.app.flags.FLAGS
 batch_size = 100
+
+def weight_variable(shape):
+  initial = tf.truncated_normal(shape, stddev=0.1)
+  return tf.Variable(initial)
+
+def bias_variable(shape):
+  initial = tf.constant(0.1, shape=shape)
+  return tf.Variable(initial)
 
 def main(_):
   ps_hosts = FLAGS.ps_hosts.split(",")
@@ -32,6 +41,7 @@ def main(_):
     server.join()
   elif FLAGS.job_name == "worker":
     mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
+    is_chief = (FLAGS.task_index == 1)
 
     # Assigns ops to the local worker by default.
     with tf.device(tf.train.replica_device_setter(
@@ -40,10 +50,21 @@ def main(_):
 
       # Build model...
       x = tf.placeholder(tf.float32, [None, 784])
-      W = tf.Variable(tf.zeros([784, 10]))
-      b = tf.Variable(tf.zeros([10]))
-      y = tf.matmul(x, W) + b
       y_ = tf.placeholder(tf.float32, [None, 10])
+
+      # Small model
+      # W = tf.Variable(tf.zeros([784, 10]))
+      # b = tf.Variable(tf.zeros([10]))
+      # y = tf.matmul(x, W) + b
+      # y_ = tf.placeholder(tf.float32, [None, 10])
+
+      # Large model
+      W1 = weight_variable([784, 100])
+      b1 = bias_variable([100])
+      y1 = tf.nn.relu(tf.matmul(x, W1) + b1)
+      W2 = weight_variable([100, 10])
+      b2 = bias_variable([10])
+      y = tf.matmul(y1, W2) + b2
 
       loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(y, y_))
       global_step = tf.Variable(0)
@@ -55,8 +76,11 @@ def main(_):
       summary_op = tf.summary.merge_all()
       init_op = tf.global_variables_initializer()
 
+      correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+      accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
     # Create a "supervisor", which oversees the training process.
-    sv = tf.train.Supervisor(is_chief=(FLAGS.task_index == 0),
+    sv = tf.train.Supervisor(is_chief=is_chief,
                              logdir="/tmp/train_logs",
                              init_op=init_op,
                              summary_op=summary_op,
@@ -70,12 +94,15 @@ def main(_):
       # Loop until the supervisor shuts down or 1000000 steps have completed.
       print "Start loop"
       step = 0
-      while not sv.should_stop() and step < 1000:
+      while not sv.should_stop() and step < FLAGS.steps:
         # Run a training step asynchronously.
         # See `tf.train.SyncReplicasOptimizer` for additional details on how to
         # perform *synchronous* training.
         batch = mnist.train.next_batch(batch_size)
         _, step = sess.run([train_op, global_step], feed_dict={x: batch[0], y_: batch[1]})
+
+      print "Training Complete. Check accuracy"
+      print(sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
 
     # Ask for all the services to stop.
     print "Training Complete. Ask for all the service to stop"
