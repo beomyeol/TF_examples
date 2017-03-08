7954c16c55af49ac20d616230814024f72b3b0a6
Beomyeol Jeon <beomyeolj@gmail.com>
2017-03-07 19:58:53 -0600

    Set name_scope for variables

--- basic/trainer_v1.0.py
+++ basic/trainer_v1.0.py
@@ -6,14 +6,14 @@ import tensorflow as tf
 
 from tensorflow.examples.tutorials.mnist import input_data
 
-def weight_variable(shape):
+def weight_variable(shape, name=None):
   initial = tf.truncated_normal(shape, stddev=0.1)
   #initial = tf.constant(0.1, shape=shape)
-  return tf.Variable(initial)
+  return tf.Variable(initial, name=name)
 
-def bias_variable(shape):
+def bias_variable(shape, name=None):
   initial = tf.constant(0.1, shape=shape)
-  return tf.Variable(initial)
+  return tf.Variable(initial, name=name)
 
 def conv2d(x, W):
   return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
@@ -43,6 +43,7 @@ def main(_):
 
     ps_device = '/job:ps'
     if FLAGS.ps_on_cpu:
+      tf.logging.info('Place ps operations on CPU')
       ps_device += '/cpu'
 
     # Assigns ops to the local worker by default.
@@ -56,22 +57,25 @@ def main(_):
       # Build model. CNN for MNIST
       x = tf.placeholder(tf.float32, [None, 784])
 
-      W_conv1 = weight_variable([5, 5, 1, 32])
-      b_conv1 = bias_variable([32])
+      with tf.name_scope('Conv1'):
+        W_conv1 = weight_variable([5, 5, 1, 32], name='weight')
+        b_conv1 = bias_variable([32], name='bias')
 
       x_image = tf.reshape(x, [-1,28,28,1])
 
       h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
       h_pool1 = max_pool_2x2(h_conv1)
 
-      W_conv2 = weight_variable([5, 5, 32, 64])
-      b_conv2 = bias_variable([64])
+      with tf.name_scope('Conv2'):
+        W_conv2 = weight_variable([5, 5, 32, 64], name='weight')
+        b_conv2 = bias_variable([64], name='bias')
 
       h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
       h_pool2 = max_pool_2x2(h_conv2)
 
-      W_fc1 = weight_variable([7 * 7 * 64, 1024])
-      b_fc1 = bias_variable([1024])
+      with tf.name_scope('Fc1'):
+        W_fc1 = weight_variable([7 * 7 * 64, 1024], name='weight')
+        b_fc1 = bias_variable([1024], name='bias')
 
       h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
       h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
@@ -79,8 +83,9 @@ def main(_):
       keep_prob = tf.placeholder(tf.float32)
       h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
 
-      W_fc2 = weight_variable([1024, 10])
-      b_fc2 = bias_variable([10])
+      with tf.name_scope('Fc2'):
+        W_fc2 = weight_variable([1024, 10], name='weight')
+        b_fc2 = bias_variable([10], name='bias')
 
       y_conv = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
       y_ = tf.placeholder(tf.float32, [None, 10])
@@ -114,7 +119,7 @@ def main(_):
 
     # LoggingHook
     hooks.append(tf.train.LoggingTensorHook(logging_tensors,
-                                            every_n_iter=1))
+                                            every_n_iter=10))
 
     # Summary writer
     hooks.append(tf.train.SummarySaverHook(save_steps=100,
