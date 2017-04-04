b1bbfa0aa3304f964711c94d68475129b7de9e42
Beomyeol Jeon <beomyeolj@gmail.com>
2017-04-04 10:47:26 -0500

    Update the MNIST example

--- basic/tf_mnist.py
+++ basic/tf_mnist.py
@@ -13,7 +13,8 @@ if __name__ == "__main__":
   y = tf.matmul(x, W) + b
   y_ = tf.placeholder(tf.float32, [None, 10])
 
-  cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(y, y_))
+  # cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=y, labels=y_))
+  cross_entropy = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=y, labels=y_))
 
   train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
   init = tf.global_variables_initializer()
@@ -21,13 +22,16 @@ if __name__ == "__main__":
   sess = tf.InteractiveSession(config=tf.ConfigProto(log_device_placement=True))
   sess.run(init)
 
-  for i in range(1000):
-    batch_xs, batch_ys = mnist.train.next_batch(100)
-    sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
+  log_period = 1
 
   correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
   accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
-  print(sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
+  for i in range(1000):
+    batch_xs, batch_ys = mnist.train.next_batch(100)
+    sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
+    if i % log_period == 0:
+      print 'step=%d, accuracy=%f' % (i,
+        sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
 
   sess.close()
